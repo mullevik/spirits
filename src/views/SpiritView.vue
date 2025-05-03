256c2d436cb13e699bf5949e2a2acf9d8ec9d3f7
Viktor Müller <muller.viktor.muller@gmail.com>
2025-05-03 18:27:35 +0200

    Add tailwind css

--- src/views/SpiritView.vue
+++ src/views/SpiritView.vue
@@ -79,5 +79,17 @@ const base = import.meta.env.BASE_URL
         <Button icon="pi pi-compass" label="Capture"></Button>
       </RouterLink>
     </div>
+
+    <div class="card flex flex-col items-center gap-4">
+      <div class="flex flex-wrap gap-4 justify-center">
+        <Button icon="pi pi-home" aria-label="Save" />
+        <Button label="Profile" icon="pi pi-user" />
+        <Button label="Save" icon="pi pi-check" iconPos="right" />
+      </div>
+      <div class="flex flex-wrap gap-4 justify-center">
+        <Button label="Search" icon="pi pi-search" iconPos="top" />
+        <Button label="Update" icon="pi pi-refresh" iconPos="bottom" />
+      </div>
+    </div>
   </main>
 </template>
